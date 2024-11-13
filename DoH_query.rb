305ce6717e2f1655ff358b36a60eb8e82b68b061
Gness Erquint <gness.na@gmail.com>
2024-11-13 21:03:39 +0300

    Compartmentalized functions. Generalize recursion in `wire_codec`.

--- DoH_query.rb
+++ DoH_query.rb
@@ -7,54 +7,65 @@ require 'resolv'
 require 'base64'
 # require 'G:\Projects\rb\Hyperspector\Hyperspector.rb'
 
+Record_types = {
+  ?4 => Resolv::DNS::Resource::IN::A,
+  ?6 => Resolv::DNS::Resource::IN::AAAA
+}
+
+doh_address = ARGV[0]
+queried_hostname = ARGV[1] || Defaults[:queried_hostname]
+record_type = ARGV[2] || ?4
+record_type = Record_types[record_type]
+
 Defaults = {
   queried_hostname: 'example.com',
-  query_type: Resolv::DNS::Resource::IN::A
+  query_type: Resolv::DNS::Resource::IN::A,
+  port: 443,
+  doh_address: doh_address
 }
 
 def wire_codec(
     hostname: Defaults[:queried_hostname],
     type: Defaults[:query_type],
-    wire64: nil
+    wire64: nil,
+    recursion: true
   )
   if wire64 then
     return Resolv::DNS::Message.decode(wire64)
   else
     message = Resolv::DNS::Message.new()
-    message.rd = 1 # Recursion desired
+    message.rd = recursion ? 1 : 0 # Recursion desired
     message.add_question(hostname, type)
     return message.encode()
   end
 end
 
-doh_server_address = ARGV[0]
-queried_hostname = ARGV[1] || Defaults[:queried_hostname]
-record_type = ARGV[2] || ?4
-
-Record_types = {
-  ?4 => Resolv::DNS::Resource::IN::A,
-  ?6 => Resolv::DNS::Resource::IN::AAAA
-}
-record_type = Record_types[record_type]
-
-wire_query = wire_codec(hostname: queried_hostname, type: record_type)
-wire64_query = Base64.urlsafe_encode64(wire_query).delete(?=)
-
-http = Net::HTTP.new(?*, 443)
-http.use_ssl = true
-http.ssl_version = :TLSv1_2
-http.verify_hostname = false
-http.ipaddr = doh_server_address
-http_get_options = {'accept' => 'application/dns-message'}
-response = http.get('/dns-query?dns=' + wire64_query, http_get_options)
-
-if response.code.to_i == 200
-  response = wire_codec(wire64: response.body)
-else
-  raise("Failed to query DoH server: #{response.code} #{response.message}")
+def get_doh_response(connection, wire64_query: nil)
+  https_get_options = {'accept' => 'application/dns-message'}
+  return  connection.get('/dns-query?dns=' + wire64_query, https_get_options)
 end
 
-if response then
+def prepare_connection(
+    port: Defaults[:port],
+    doh_address: Defaults[:doh_address]
+  )
+  https_connection = Net::HTTP.new(?*, port)
+  https_connection.use_ssl = true
+  https_connection.ssl_version = :TLSv1_2
+  https_connection.verify_hostname = false
+  https_connection.ipaddr = doh_address
+  return https_connection
+end
+
+def print_doh_response(response)
+  raise('response object falsey') unless response
+  
+  if response.code.to_i == 200
+    response = wire_codec(wire64: response.body)
+  else
+    raise("Failed to query DoH server: #{response.code} #{response.message}")
+  end
+  
   puts('Requests:')
   response.question.each() do |question|
     question.each() do |field|
@@ -70,4 +81,12 @@ if response then
       puts("  TTL:\n    #{field.ttl()}") if field.respond_to?(:ttl)
     end
   end
+  
+  return nil
 end
+
+wire_query = wire_codec(hostname: queried_hostname, type: record_type)
+wire64_query = Base64.urlsafe_encode64(wire_query).delete(?=)
+connection = prepare_connection()
+response = get_doh_response(connection, wire64_query: wire64_query)
+print_doh_response(response)
