815b923b008d351ced7d746ba17ba4ae796eef57
Gness Erquint <gness.na@gmail.com>
2024-11-13 07:41:50 +0300

    DNSless DoH implemented.

--- DoH_query.rb
+++ DoH_query.rb
@@ -3,63 +3,76 @@ Encoding.default_external = Encoding::ASCII_8BIT
 Encoding.default_internal = Encoding::ASCII_8BIT
 
 require 'net/http'
-require 'uri'
-require 'base64'
 require 'resolv'
-# require 'G:\Projects\rb\Hyperspector.rb'
+require 'base64'
+# require 'G:\Projects\rb\Hyperspector\Hyperspector.rb'
 
-module DNS_wire_format
-  def self.encode(domain, type)
+Defaults = {
+  queried_hostname: 'example.com',
+  query_type: Resolv::DNS::Resource::IN::A
+}
+
+def wire_codec(
+    hostname: Defaults[:queried_hostname],
+    type: Defaults[:query_type],
+    wire64: nil
+  )
+  if wire64 then
+    return Resolv::DNS::Message.decode(wire64)
+  else
     message = Resolv::DNS::Message.new()
     message.rd = 1 # Recursion desired
-    message.add_question(domain, type)
+    message.add_question(hostname, type)
     return message.encode()
   end
 end
 
-def query_DoH(server_url, domain, type = Resolv::DNS::Resource::IN::A)
-  query = DNS_wire_format.encode(domain, type)
-  encoded_query = Base64.urlsafe_encode64(query).delete('=')
+doh_server_address = ARGV[0]
+queried_hostname = ARGV[1] || Defaults[:queried_hostname]
+record_type = ARGV[2] || ?4
 
-  uri = URI.parse("#{server_url}?dns=#{encoded_query}")
-  http = Net::HTTP.new(uri.host, uri.port)
-  http.use_ssl = true if uri.scheme == 'https'
+Record_types = {
+  ?4 => Resolv::DNS::Resource::IN::A,
+  ?6 => Resolv::DNS::Resource::IN::AAAA
+}
+record_type = Record_types[record_type]
 
-  request = Net::HTTP::Get.new(uri.request_uri)
-  request['accept'] = 'application/dns-message'
+wire_query = wire_codec(hostname: queried_hostname, type: record_type)
+wire64_query = Base64.urlsafe_encode64(wire_query).delete(?=)
 
-  response = http.request(request)
+http_connection_options = {
+  use_ssl: true,
+  ssl_version: :TLSv1_2,
+  verify_hostname: false,
+  ipaddr: doh_server_address
+}
+http_get_options = {'accept' => 'application/dns-message'}
 
-  if response.code.to_i == 200
-    return response.body
-  else
-    puts "Failed to query DoH server: #{response.code} #{response.message}"
-    return nil
-  end
+response = nil
+http = Net::HTTP.start(?*, 443, nil, nil, nil, nil, http_connection_options) do |connection|
+  response = connection.get('/dns-query?dns=' + wire64_query, http_get_options)
 end
 
-server_url = ARGV[0]
-domain = ARGV[1]
-
-response = query_DoH(server_url, domain)
+if response.code.to_i == 200
+  response = wire_codec(wire64: response.body)
+else
+  raise("Failed to query DoH server: #{response.code} #{response.message}")
+end
 
 if response then
-  decoded_response = Resolv::DNS::Message.decode(response)
   puts('Requests:')
-  decoded_response.question.each() do |question|
+  response.question.each() do |question|
     question.each() do |field|
       puts("  Hostname:\n    #{field}") if field.is_a?(Resolv::DNS::Name)
-      puts("  Class:\n    #{field}") if field.is_a?(Class)
+      puts("  Class:\n    #{field.to_s().split(?:).last()}") if field.is_a?(Class)
     end
   end
-
+  
   puts('Responses:')
-  decoded_response.answer.each() do |answer|
+  response.answer.each() do |answer|
     answer.each() do |field|
       puts("  Address:\n    #{field.address()}") if field.respond_to?(:address)
       puts("  TTL:\n    #{field.ttl()}") if field.respond_to?(:ttl)
     end
   end
 end
-
-__END__
