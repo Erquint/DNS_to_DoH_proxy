4a5e9a967148cced0862cda0cd5ca3de8d5d1745
Gness Erquint <gness.na@gmail.com>
2024-11-14 22:38:27 +0300

    Implemented DoH over DNS local proxy.

--- DoH_query.rb
+++ DoH_query.rb
@@ -4,61 +4,16 @@ Encoding.default_internal = Encoding::ASCII_8BIT
 
 require 'net/http'
 require 'resolv'
-# require 'G:\Projects\rb\Hyperspector\Hyperspector.rb'
-
-Record_types = {
-  ?4 => Resolv::DNS::Resource::IN::A,
-  ?6 => Resolv::DNS::Resource::IN::AAAA
-}
-
-doh_address = ARGV[0]
-queried_hostname = ARGV[1]
-record_type = ARGV[2] || ?4
-record_type = Record_types[record_type]
+require 'socket'
 
 Defaults = {
-  queried_hostname: 'example.com',
-  query_type: Resolv::DNS::Resource::IN::A,
+  doh_address: '1.1.1.1',
   doh_port: 443,
-  doh_address: doh_address,
+  dns_address: '0.0.0.0',
+  dns_port: 53,
   path: '/dns-query',
   parameter: 'dns'
 }
-
-queried_hostname = Defaults[:queried_hostname]
-re_match = doh_address.match(/(?<!\d)((?:\d{1,3}\.){3}\d{1,3})(?::(\d{1,5}))?(?!\d)/)
-doh_address = re_match[1]
-doh_port = (re_match[2] || Defaults[:doh_port]).to_i()
-
-def wire_codec(
-    hostname: Defaults[:queried_hostname],
-    type: Defaults[:query_type],
-    wirecode: nil,
-    recursion: true,
-    b64: false
-  )
-  if wirecode then
-    wirecode = Base64.urlsafe_decode64(wirecode) if b64
-    return Resolv::DNS::Message.decode(wirecode)
-  else
-    wirecode = Resolv::DNS::Message.new()
-    wirecode.rd = recursion ? 1 : 0 # Recursion desired
-    wirecode.add_question(hostname, type)
-    wirecode =-wirecode.encode()
-    wirecode = Base64.urlsafe_encode64(wirecode).delete(?=) if b64
-    return wirecode
-  end
-end
-
-def doh_get_response(connection, wire64_message)
-  https_get_headers = {'accept' => 'application/dns-message'}
-  path = Defaults[:path] + ?? + Defaults[:parameter] + ?= + wire64_message
-  begin
-    return connection.get(path, https_get_headers)
-  rescue => exception
-    return exception
-  end
-end
 
 def doh_post_response(connection, wire_message)
   https_post_headers = {
@@ -66,55 +21,82 @@ def doh_post_response(connection, wire_message)
     'Accept' => 'application/dns-message'
   }
   begin
-    return connection.post(Defaults[:path], wire_message, https_post_headers)
+    response = connection.post(Defaults[:path], wire_message, https_post_headers)
   rescue => exception
-    return exception
+    response = exception
   end
+  
+  return(response)
 end
 
-def prepare_connection(
-    doh_address: Defaults[:doh_address],
-    doh_port: Defaults[:doh_port]
-  )
+def prepare_doh_connection(doh_address: Defaults[:doh_address], doh_port: Defaults[:doh_port])
   https_connection = Net::HTTP.new(?*, doh_port)
   https_connection.use_ssl = true
   https_connection.ssl_version = :TLSv1_2
   https_connection.verify_hostname = false
   https_connection.ipaddr = doh_address
-  return https_connection
+  
+  return(https_connection)
 end
 
-def print_doh_response(response)
-  raise('TLS connection failed to establish!') if response.is_a?(OpenSSL::SSL::SSLError)
-  
-  if response.code.to_i == 200
-    response = wire_codec(wirecode: response.body)
+def print_doh_response(doh_response_body_decoded)
+  if doh_response_body_decoded.answer.any?() then
+    puts('Resolved:')
+    pp(doh_response_body_decoded.answer)
   else
-    raise("Failed to query DoH server: #{response.code} #{response.message}!")
+    puts('Unresolved:')
+    pp(doh_response_body_decoded.question)
   end
   
-  puts('Requests:')
-  response.question.each() do |question|
-    question.each() do |field|
-      puts("  Hostname:\n    #{field}") if field.is_a?(Resolv::DNS::Name)
-      puts("  Class:\n    #{field.to_s().split(?:).last()}") if field.is_a?(Class)
-    end
-  end
-  
-  puts('Responses:')
-  response.answer.each() do |answer|
-    answer.each() do |field|
-      puts("  Address:\n    #{field.address()}") if field.respond_to?(:address)
-      puts("  TTL:\n    #{field.ttl()}") if field.respond_to?(:ttl)
-    end
-  end
-  
-  return nil
+  return(nil)
 end
 
-connection = prepare_connection(doh_address: doh_address, doh_port: doh_port)
-# wire64_message = wire_codec(hostname: queried_hostname, type: record_type, b64: true)
-# response = doh_get_response(connection, wire64_message)
-wire_message = wire_codec(hostname: queried_hostname, type: record_type)
-response = doh_post_response(connection, wire_message)
-print_doh_response(response)
+def serve_dns_doh_proxy(
+    dns_address: Defaults[:dns_address],
+    dns_port: Defaults[:dns_port],
+    doh_address: Defaults[:doh_address],
+    doh_port: Defaults[:doh_port]
+  )
+  
+  begin
+    socket = UDPSocket.new
+    socket.bind(dns_address, dns_port)
+    
+    puts "DNS server started on port #{dns_port}"
+    
+    while (dns_request, sender_addrinfo = socket.recvfrom(512)).first() do
+      sender_class, sender_port, sender_address_label, sender_address = sender_addrinfo
+      puts("Queried from \"#{sender_address_label}\" on #{sender_address}:#{sender_port}.")
+      doh_connection = prepare_doh_connection(doh_address: doh_address, doh_port: doh_port)
+      doh_response = doh_post_response(doh_connection, dns_request)
+      raise('TLS connection failed to establish!') if doh_response.is_a?(OpenSSL::SSL::SSLError)
+      print_doh_response(Resolv::DNS::Message.decode(doh_response.body))
+      socket.send(doh_response.body, 0, sender_address, sender_port)
+    end
+    
+    raise(<<~HEREDOC) if [0, nil].include?(dns_request)
+      dns_request: #{dns_request.inspect()}
+      When recvfrom(2) returns 0,
+      Socket#recv_nonblock returns nil. In most cases it means the connection 
+      was closed, but it may also mean an empty packet was received, as the 
+      underlying API makes it impossible to distinguish these two cases.
+      HEREDOC
+    raise('Serving loop broke for unknown reason.')
+  rescue => exception
+    pp(exception, 'Re-establishing a socket binding and serving again…')
+    puts(<<~HEREDOC) if exception.is_a?(Errno::ECONNRESET)
+      On a UDP-datagram socket this error indicatesa previous send operation
+      resulted in an ICMP Port Unreachable message.
+      HEREDOC
+    socket.close()
+    retry unless exception.is_a?(Interrupt)
+  end
+  
+  return(nil)
+end
+
+doh_full_address = ARGV[0]
+re_match = doh_full_address.match(/(?<!\d)((?:\d{1,3}\.){3}\d{1,3})(?::(\d{1,5}))?(?!\d)/)
+doh_address = re_match[1]
+doh_port = (re_match[2] || Defaults[:doh_port]).to_i()
+serve_dns_doh_proxy(dns_address: '0.0.0.0', doh_address: doh_address, doh_port: doh_port)
