1fa8272ba468cbb721f480a13c4c20d5cf6d747c
Gness Erquint <gness.na@gmail.com>
2024-11-21 23:47:55 +0300

    Threaded networking. Also sped up single-threaded operation somehow.

--- dns_server.rb
+++ dns_server.rb
@@ -15,60 +15,102 @@ module DNS_to_DoH_proxy
       local_address_arpa: Defaults[:local_address_arpa]
     )
     
-    begin
-      socket = UDPSocket.new()
-      socket.bind(dns_address, dns_port)
-      puts("DNS server bound to #{dns_address}:#{dns_port}", nil)
-      doh_connection = prepare_doh_connection(doh_address: doh_address, doh_port: doh_port)
+    doh_connection = prepare_doh_connection(doh_address: doh_address, doh_port: doh_port)
+    socket = UDPSocket.new()
+    socket.bind(dns_address, dns_port)
+    puts("DNS server bound to #{dns_address}:#{dns_port}", nil)
+    Thread.report_on_exception = false
+    Thread.abort_on_exception = false
+    threads = Array.new()
+    socket_mutex = Thread::Mutex.new()
       
+    begin
       while (dns_message, sender_addrinfo = socket.recvfrom(512)).first() do
-        time = Time.now()
-        handled_locally = false
-        sender_class, sender_port, sender_address_label, sender_address = sender_addrinfo
-        
-        dns_message_decoded = Resolv::DNS::Message.decode(dns_message)
-        if dns_message_decoded.question()[0][1] == Resolv::DNS::Resource::IN::PTR &&
-          dns_message_decoded.question()[0][0].to_s().include?('1.0.0.127.in-addr.arpa') ||
-          dns_message_decoded.question()[0][0].to_s().include?(local_address_arpa) ||
-          dns_message_decoded.question()[0][0].to_s().include?('7.3.3.1.in-addr.arpa') ||
-          dns_message_decoded.question()[0][0].to_s().include?("1.0.0.0#{'.0'*24}.0.0.0.0.ip6.arpa") ||
-          # ↓ Arpa lookup for for mock IPv6 address fe80::100.
-          dns_message_decoded.question()[0][0].to_s().include?("0.0.1.0#{'.0'*24}.0.8.e.f.ip6.arpa") ||
-          dns_message_decoded.question()[0][0].to_s().include?("7.3.1.1#{'.0'*24}.0.0.0.0.ip6.arpa") then
+        threads << Thread.start(dns_message.dup(), sender_addrinfo.dup(), doh_connection.dup()) do |dns_message, sender_addrinfo, doh_connection|
+          time = Time.now()
+          handled_locally = false
+          sender_class, sender_port, sender_address_label, sender_address = sender_addrinfo
           
-          dns_message_decoded.add_answer(
-            dns_message_decoded.question()[0][0], 1,
-            Resolv::DNS::Resource::IN::PTR.new(Resolv::DNS::Name.create('erquint.leet.'))
-          )
-          handled_locally = true
-        elsif dns_message_decoded.question()[0][0].to_s() == 'erquint.leet' then
-          if dns_message_decoded.question()[0][1] == Resolv::DNS::Resource::IN::A
+          dns_message_decoded = Resolv::DNS::Message.decode(dns_message)
+          
+          Thread.current.name = dns_message_decoded.id().to_s()
+          Thread.current[:dns_message] = dns_message
+          Thread.current[:sender_addrinfo] = sender_addrinfo
+          
+          if dns_message_decoded.question()[0][1] == Resolv::DNS::Resource::IN::PTR &&
+            dns_message_decoded.question()[0][0].to_s().include?('1.0.0.127.in-addr.arpa') ||
+            dns_message_decoded.question()[0][0].to_s().include?(local_address_arpa) ||
+            dns_message_decoded.question()[0][0].to_s().include?('7.3.3.1.in-addr.arpa') ||
+            dns_message_decoded.question()[0][0].to_s().include?("1.0.0.0#{'.0'*24}.0.0.0.0.ip6.arpa") ||
+            # ↓ Arpa lookup for for mock IPv6 address fe80::100.
+            dns_message_decoded.question()[0][0].to_s().include?("0.0.1.0#{'.0'*24}.0.8.e.f.ip6.arpa") ||
+            dns_message_decoded.question()[0][0].to_s().include?("7.3.1.1#{'.0'*24}.0.0.0.0.ip6.arpa") then
+            
             dns_message_decoded.add_answer(
               dns_message_decoded.question()[0][0], 1,
-              Resolv::DNS::Resource::IN::A.new('1.3.3.7')
+              Resolv::DNS::Resource::IN::PTR.new(Resolv::DNS::Name.create('erquint.leet.'))
             )
-          elsif dns_message_decoded.question()[0][1] == Resolv::DNS::Resource::IN::AAAA then
-            dns_message_decoded.add_answer(
-              dns_message_decoded.question()[0][0], 1,
-              Resolv::DNS::Resource::IN::AAAA.new('::1337')
-            )
+            handled_locally = true
+          elsif dns_message_decoded.question()[0][0].to_s() == 'erquint.leet' then
+            if dns_message_decoded.question()[0][1] == Resolv::DNS::Resource::IN::A
+              dns_message_decoded.add_answer(
+                dns_message_decoded.question()[0][0], 1,
+                Resolv::DNS::Resource::IN::A.new('1.3.3.7')
+              )
+            elsif dns_message_decoded.question()[0][1] == Resolv::DNS::Resource::IN::AAAA then
+              dns_message_decoded.add_answer(
+                dns_message_decoded.question()[0][0], 1,
+                Resolv::DNS::Resource::IN::AAAA.new('::1337')
+              )
+            end
+            handled_locally = true
+          else
+            doh_response = doh_post(doh_connection, dns_message)
+            raise('TLS connection failed to establish!') if doh_response.is_a?(OpenSSL::SSL::SSLError)
+            raise("Failed to query DoH server: #{doh_response.code} #{doh_response.message}!") unless doh_response.code.to_i() == 200
+            dns_message = doh_response.body()
           end
-          handled_locally = true
-        else
-          doh_response = doh_post(doh_connection, dns_message)
-          raise('TLS connection failed to establish!') if doh_response.is_a?(OpenSSL::SSL::SSLError)
-          raise("Failed to query DoH server: #{doh_response.code} #{doh_response.message}!") unless doh_response.code.to_i() == 200
-          dns_message = doh_response.body()
+          
+          if handled_locally then
+              print_dns_message(dns_message_decoded, sender_addrinfo, time)
+              dns_message = dns_message_decoded.encode()
+          else
+            print_dns_message(dns_message, sender_addrinfo, time)
+          end
+          
+          socket_mutex.synchronize() do
+            socket.send(dns_message, 0, sender_address, sender_port)
+          end
         end
         
-        if handled_locally then
-          print_dns_message(dns_message_decoded, sender_addrinfo, time)
-          dns_message = dns_message_decoded.encode()
-        else
-          print_dns_message(dns_message, sender_addrinfo, time)
-        end
+        threads, dead_threads = threads.partition(&:alive?)
         
-        socket.send(dns_message, 0, sender_address, sender_port)
+        dead_threads.each() do |dead_thread|
+          begin
+            dead_thread.join()
+          rescue Exception => exception
+            sender_class, sender_port, sender_address_label, sender_address = dead_thread[:sender_addrinfo]
+            
+            if dead_thread[:dns_message] then
+              dns_message_decoded = Resolv::DNS::Message.decode(dead_thread[:dns_message])
+              dns_message_decoded.rcode = 2
+              print_dns_message(dns_message_decoded, sender_addrinfo, time)
+              dns_message = dns_message_decoded.encode()
+              
+              socket_mutex.synchronize() do
+                socket.send(dns_message, 0, sender_address, sender_port)
+              end
+            end
+            
+            puts(<<~HEREDOC)
+              Failed to process message/thread ID:
+              #{dead_thread.name} for client #{dead_thread[:sender_address]}:#{dead_thread[:sender_port]}
+              and returned a DNS error code.
+              Exception:
+              #{exception.full_message(highlight: true)}
+            HEREDOC
+          end
+        end
       end
       
       raise("Connection closed.\ndns_message: #{dns_message.inspect()}") if dns_message.zero?
@@ -77,9 +119,6 @@ module DNS_to_DoH_proxy
       if exception.is_a?(Interrupt) then
         puts('Exiting gracefully…')
         exit(0)
-      elsif exception.is_a?(Errno::EADDRINUSE) then
-        puts(exception)
-        exit(1)
       elsif exception.is_a?(Errno::ECONNRESET) then
         puts(<<~HEREDOC)
           #{exception}
@@ -90,17 +129,15 @@ module DNS_to_DoH_proxy
         puts(exception.full_message(highlight: true))
       end
       
-      if defined?(socket) && socket && !(socket.closed?()) then
-        if defined?(dns_message) && dns_message && dns_message.is_a?(String) then
-          dns_message_decoded = Resolv::DNS::Message.decode(dns_message)
-          dns_message_decoded.rcode = 2
-          dns_message = dns_message_decoded.encode()
-          socket.send(dns_message, 0, sender_address, sender_port)
-        end
-        socket.close()
+      puts('Re-establishing a socket binding…')
+      
+      socket_mutex.synchronize() do
+        socket.close() unless socket.closed?()
+        socket = UDPSocket.new()
+        socket.bind(dns_address, dns_port)
       end
       
-      puts('Re-establishing a socket binding and serving again…')
+      puts('Serving again…')
       
       retry
     end
